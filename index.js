1cdfa5309ae14ddde96004188f95ceb23898bf24
claudine-001 <claudine.kimani@student.moringaschool.com>
2022-11-25 01:04:24 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(defaultArgument="roller-skate"){
+    return `This Saturday, I want to ${defaultArgument}!`
+}
+
+function mondayWork(defaultArgument='go to the office'){
+    return `This Monday, I will ${defaultArgument}.`
+
+}
+
+function wrapAdjective(magic= "||"){
+    return function(adjective = "a dedicated programmer "){
+        return `You are ${magic}${adjective}${magic}!`
+    }
+}
